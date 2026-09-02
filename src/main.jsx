cdc5f43750f3d8ac377b8b79b0a608043a9e2145
abelkidus <abelhadush18@gmail.com>
2026-09-02 22:14:15 +0300

    refactor(client): wrap application with auth provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { Toaster } from "react-hot-toast";
+import { AuthProvider } from "./context/AuthContext";
 import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-      <Toaster position="top-right" reverseOrder={false} />
-      <App />
+      <AuthProvider>
+        <Toaster position="top-right" reverseOrder={false} />
+        <App />
+      </AuthProvider>
     </GoogleOAuthProvider>
   </StrictMode>,
 );

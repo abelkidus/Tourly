3a68a4536d787fb9811111d490fa3d9d7f6f7553
abelkidus <abelhadush18@gmail.com>
2026-09-02 22:06:44 +0300

    feat(client): install react-hot-toast and mount global toaster

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import { Toaster } from "react-hot-toast";
 import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+      <Toaster position="top-right" reverseOrder={false} />
       <App />
     </GoogleOAuthProvider>
   </StrictMode>,
 );
+
